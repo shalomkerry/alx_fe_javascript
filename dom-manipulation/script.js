97a3970bbf357a0887304cf689d0dc32b2fedc95
Shalom <youremail@gmail.com>
2024-10-26 16:54:16 +0300

    everything

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -16,12 +16,12 @@ let quotes = [
 
 const button = document.getElementById("newQuote");
 const showingArea = document.getElementById("quoteDisplay");
-
+let i = 0;
 function showRandomQuote() {
   clearQuotes();
   let len = quotes.length;
-  let i = Math.floor(Math.random() * len);
   console.log(i);
+
   if (localStorage.length > 0) {
     quotes = JSON.parse(localStorage.getItem("quotes"));
   }
@@ -31,6 +31,10 @@ function showRandomQuote() {
 
   showingArea.appendChild(element);
   i++;
+
+  if (i >= len) {
+    i = 0;
+  }
 }
 function clearQuotes() {
   showingArea.innerHTML = "";
@@ -57,3 +61,48 @@ function createAddQuoteForm() {
 }
 
 button.addEventListener("click", showRandomQuote);
+
+function createDownloadLink() {
+  let fetchData = localStorage.getItem("quotes");
+  if (fetchData) {
+    const jsonBlob = new Blob([fetchData], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(jsonBlob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "data/json";
+    link.click();
+    URL.revokeObjectURL(url);
+  } else {
+    console.log("No Data Found in local Storage");
+  }
+}
+const downloadButton = document.getElementById("download");
+function saveQuotes() {
+  localStorage.setItem("quotes", JSON.stringify(quotes));
+}
+function importFromJsonFile(event) {
+  const file = event.target.files[0];
+  if (!file) {
+    alert("something wrong");
+    return;
+  }
+  if (file.type !== "application/json") {
+    alert("file type wrong");
+    return;
+  }
+  const fileReader = new FileReader();
+
+  fileReader.onload = function (event) {
+    try {
+      let jsonQuotes = JSON.parse(event.target.result);
+      quotes.push(...jsonQuotes);
+      saveQuotes();
+      alert("succesfully imported your files");
+    } catch (error) {
+      console.log("error message", error);
+    }
+  };
+  fileReader.readAsText(file);
+}
